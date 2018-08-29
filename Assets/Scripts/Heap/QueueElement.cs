7d46a89d3fab1943c1787be36d7ba5c49ada6a6d
Nicholas Jones <rho1.cancri@yahoo.com>
2018-08-29 08:19:14 -0400

    Added a helper class for priority queues

---
+++ Assets/Scripts/Heap/QueueElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct QueueElement<TValue> : System.IComparable {
+	// A very simple helper class for working with heaps.
+	// Combines an arbitrary value with a priority.
+	public float priority;
+	public TValue value;
+
+	public QueueElement(float priority, TValue value) {
+		this.priority = priority;
+		this.value = value;
+	}
+
+	public int CompareTo(object obj) {
+		if (obj == null || GetType() != obj.GetType()) {
+			return 0;
+		} else {
+			QueueElement<TValue> b = (QueueElement<TValue>)obj;
+			return priority.CompareTo(b.priority);
+		}
+	}
+}
